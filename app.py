3dafeaf46f1e8675abecda5781cb97af1dbfcf09
geomars <egogeomars@gmail.com>
2014-07-03 06:00:00 +0510

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 # Views
 @app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 @app.errorhandler(500)
-def internal_server_error(e):
+def internal_server_error(error):
     return render_template('500.html'), 500
     
 @app.route('/')
